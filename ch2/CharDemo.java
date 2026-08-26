a7fc23d5dd6f07d714a5052784fe9982e053fd74
sdev200fl26 <sdev200fl26@outlook.com>
2026-08-26 16:04:45 -0400

    ch2 some youdoit

---
+++ ch2/CharDemo.java
@@ -0,0 +1,10 @@
+// Bill Ruben
+// p.56
+public class CharDemo {
+    public static void main(String[] args) {
+        char initial = 'A';
+        System.out.println(initial);
+        System.out.print("\t\"abc\\def\bghi\n\njkl");
+
+    }
+}
