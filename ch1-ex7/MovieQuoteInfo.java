208e0faaa18069d1436cda2effb4856c4f312042
sdev200fl26 <sdev200fl26@outlook.com>
2026-08-31 15:42:44 -0400

    ch1-7sc

---
+++ ch1-ex7/MovieQuoteInfo.java
@@ -0,0 +1,11 @@
+// Bill Ruben
+// p.35  Exercise 7
+
+public class MovieQuoteInfo {
+    public static void main(String[] args) {
+        System.out.println("Famous quote: \"Show me the money\"");
+        System.out.println("Name of the movie: Jerry Maguire");
+        System.out.println("Name of the character: Rod Tidwell");
+        System.out.println("Year released: 1996");
+    }
+}
