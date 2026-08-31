ce6a804d4e9d39bc2a523e240cc5754134be3872
sdev200fl26 <sdev200fl26@outlook.com>
2026-08-31 15:21:45 -0400

    ch2sc

---
+++ ch2/ArithmeticDemo3.java
@@ -0,0 +1,30 @@
+// Bill Ruben
+// p.71 Aug. 31
+
+import java.util.Scanner;
+
+public class ArithmeticDemo3
+
+{
+        public static void main(String[] args) {
+                int firstNumber;
+                int secondNumber;
+                int sum;
+                int difference;
+                double average;
+                Scanner input = new Scanner(System.in);
+                System.out.print("Please enter a double >> ");
+                firstNumber = input.nextInt();
+                System.out.print("Please enter another double >> ");
+                secondNumber = input.nextInt();
+                sum = firstNumber + secondNumber;
+                difference = firstNumber - secondNumber;
+                average = (double) sum / 2;
+                System.out.println(firstNumber + " + " +
+                                secondNumber + " is " + sum);
+                System.out.println(firstNumber + " - " +
+                                secondNumber + " is " + difference);
+                System.out.println("The average of " + firstNumber +
+                                " and " + secondNumber + " is " + average);
+        }
+}
