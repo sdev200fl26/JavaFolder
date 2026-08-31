6b749a59b86d2d70b1135ca616af5fc01f521fa2
sdev200fl26 <sdev200fl26@outlook.com>
2026-08-31 14:32:09 -0400

    chapter 1

---
+++ ch1/Hello2.java
@@ -0,0 +1,11 @@
+// Bill Ruben
+// p.17
+// Filename Hello2.java
+// Written on August 31, 2026
+public class Hello2 {
+/* This class demonstrates the use of the println() 
+method to print the message Hello, world! */
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
---
+++ ch1/HelloDialog.java
@@ -0,0 +1,10 @@
+
+// Bill Ruben
+// p.29  Aug. 31
+import javax.swing.JOptionPane;
+
+public class HelloDialog {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hello, world!");
+    }
+}
