40c699dc64fc4b961c988e73410e9ae2549bbb7d
sdev200fl26 <sdev200fl26@outlook.com>
2026-09-06 19:53:24 -0400

    ch3 changes

---
+++ ch3/ch3YouDoIt/TestInfo.java
@@ -0,0 +1,8 @@
+// Bill Ruben
+// p.90
+public class TestInfo {
+    public static void main(String[] args) {
+        System.out.println("Calling method from another class:");
+        ParadiseInfo.displayInfo();
+    }
+}
