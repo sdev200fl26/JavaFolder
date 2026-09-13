2323007dd5ffe0bdbddff839f380c091724793e8
sdev200fl26 <sdev200fl26@outlook.com>
2026-09-12 22:21:45 -0400

    ch5ydit

---
+++ ch4/ch4YouDoIt/SpaService.java
@@ -0,0 +1,31 @@
+// Bill Ruben
+// p.133
+public class SpaService {
+    private String serviceDescription;
+    private double price;
+
+    public SpaService() {
+        serviceDescription = "XXX";
+        price = 0;
+    }
+    public SpaService(String desc, double pr)
+    {
+        serviceDescription = desc;
+        price = pr;
+    }
+    public void setServiceDescription(String service) {
+        serviceDescription = service;
+    }
+
+    public void setPrice(double servicePrice) {
+        price = servicePrice;
+    }
+
+    public String getServiceDescription() {
+        return serviceDescription;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
---
+++ ch5/ch5youdoit/ShortCircuitTestOr.java
@@ -0,0 +1,20 @@
+// Bill Ruben
+// p.176
+public class ShortCircuitTestOr {
+    public static void main(String[] args) {
+        if (trueMethod() || falseMethod())
+            System.out.println("Both are true");
+        else
+            System.out.println("Both are not true");
+    }
+
+    public static boolean trueMethod() {
+        System.out.println("Within trueMethod()");
+        return true;
+    }
+
+    public static boolean falseMethod() {
+        System.out.println("Within falseMethod()");
+        return false;
+    }
+}
